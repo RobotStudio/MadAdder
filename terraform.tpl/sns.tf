2c6d13bb3aaf8f341d56b4e9199f8035afb724ed
Bobby <karma0@gmail.com>
2019-02-12 16:36:47 -0700

    Added all identities and conditional rule to policy

--- terraform.tpl/sns.tf
+++ terraform.tpl/sns.tf
@@ -11,10 +11,23 @@ data "aws_iam_policy_document" "sns_{{app_name}}" {
   statement {
     effect = "Allow",
     actions = ["SNS:Publish"]
+
     principals {
       type = "Service"
-      identifiers = ["events.amazonaws.com"]
+      identifiers = ["*"]
     }
+
+    condition {
+      test = "ArnEquals"
+      variable = "aws:SourceArn"
+
+      values = [
+        "${aws_lambda_function.{{app_name}}_seconds_notifier.arn}",
+        {% for interval, rate in minute_intervals.items() %}
+        "${aws_cloudwatch_event_target.{{interval}}_sns}",{% endfor %}
+      ]
+    }
+
     resources = ["${aws_sns_topic.{{app_name}}.arn}"]
   }
 }
