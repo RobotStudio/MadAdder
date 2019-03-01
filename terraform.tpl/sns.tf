8457867da95db8ea9b2f4eade9683609ea02493e
Bobby <karmma0@gmail.com>
2019-02-28 20:17:26 -0700

    Opening up policies

--- terraform.tpl/sns.tf
+++ terraform.tpl/sns.tf
@@ -23,10 +23,10 @@ data "aws_iam_policy_document" "sns_{{app_name}}" {
       test = "ArnEquals"
       variable = "aws:SourceArn"
 
-      values = ["*"]
-#        "${aws_lambda_function.{{app_name}}_seconds_notifier.arn}",{% for interval, rate in minute_intervals.items() %}
-#        "${aws_cloudwatch_event_target.{{interval}}_sns.arn}",{% endfor %}
-#      ]
+      values = [
+        "${aws_lambda_function.{{app_name}}_seconds_notifier.arn}",{% for interval, rate in minute_intervals.items() %}
+        "${aws_cloudwatch_event_target.{{interval}}_sns.arn}",{% endfor %}
+      ]
     }
 
     resources = ["${aws_sns_topic.{{app_name}}.arn}"]
